5f90cc95147f3d55d595d1b1f8cb79117ed0abf3
dalton704 <lokuma96@gmail.com>
2021-11-29 11:35:24 -0800

    Update JavaScript.js

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -24,7 +24,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0, i < dots.length; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
@@ -33,7 +33,7 @@ function showSlides(n) {
 
 //This code will create close the contact form when the user clicks off of it
 document.addEventListener("click", function(event){
-    if (event.target.matches(".cencel") || !event.target.closest(."form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+    if (event.target.matches(".cencel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
         closeForm()
     }
 }, false )
